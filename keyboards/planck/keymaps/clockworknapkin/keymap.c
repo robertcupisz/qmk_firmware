3e4e68548d2ea75d5b16c85b4ac607deceec47aa
Robert Cupisz <robertcupisz@gmail.com>
2019-06-10 21:09:02 +0200

    Updated lower layer to match the image

--- keyboards/planck/keymaps/clockworknapkin/keymap.c
+++ keyboards/planck/keymaps/clockworknapkin/keymap.c
@@ -105,14 +105,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |   &  |   *  |      |      |      |      |   {  |   }  |      |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Home |      | End  |
+ * |      |      |      |      |      |             |      |      | Home |      | End  |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    KC_TILD, _______, KC_EXLM, KC_AT,   KC_HASH, _______, _______, _______,  _______,  KC_UNDS, KC_EQL, _______,
+    _______, _______, KC_DLR,  KC_PERC, KC_CIRC, _______, _______, _______,  _______, _______, _______, KC_PIPE,
+    _______, _______, KC_AMPR, KC_ASTR, _______, _______, _______, _______,  KC_LCBR, KC_RCBR, _______, KC_BSLS,
+    _______, _______, _______, _______, _______, _______, _______, _______,  _______, KC_HOME, _______, KC_END
 ),
 
 /* Raise
